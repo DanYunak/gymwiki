399ed629e608dccd4850c2459c9aa40cf397672c
DanYunak <danyloyunak2006@gmail.com>
2023-08-15 22:07:35 +0300

    Finished version

---
+++ src/app/components/App.tsx
@@ -0,0 +1,59 @@
+import { CircularProgress } from '@mui/material'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Route, Routes } from 'react-router'
+import { actions as appActions } from '../../app/model/appActions'
+import { LoginWindow } from '../../features/AuthWindow/components/LoginWindow'
+import { RegistrationWindow } from '../../features/AuthWindow/components/RegistrationWindow'
+import { actions } from '../../features/User/model/userActions'
+import { ExercisesPage } from '../../pages/Exercises/components/ExercisesPage'
+import { ProductsPage } from '../../pages/Products/components/ProductsPage'
+import { useAppDispatch } from '../../redux/store'
+import { Header } from '../../widgets/Header'
+import { getAllProducts } from '../../widgets/Products/model/productsSelectors'
+import { getIsLoading, getIsLoginWindowOpen, getIsRegistrationWindowOpen } from '../model/appSelectors'
+import './App.scss'
+
+export const App = () => {
+  const dispatch = useAppDispatch()
+
+  const isLoginWindowOpen = useSelector(getIsLoginWindowOpen)
+  const isRegistrationWindowOpen = useSelector(getIsRegistrationWindowOpen)
+  const isLoading = useSelector(getIsLoading)
+  const products = useSelector(getAllProducts)
+
+  const onSubmitLogin = (formData: any) => {
+    dispatch(actions.login(formData))
+  }
+
+  const onSubmitRegistration = (formData: any) => {
+    dispatch(actions.register(formData))
+  }
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(appActions.setIsLoading(true))
+    } else {
+      dispatch(appActions.setIsLoading(false))
+    }
+  }, [products])  
+
+  return (
+    <div className='app__container'>
+      <div className='app'>
+        {isLoading &&
+          <div className='loading'>
+            <CircularProgress />
+          </div>
+        }
+        {isLoginWindowOpen && <LoginWindow onSubmit={onSubmitLogin} />}
+        {isRegistrationWindowOpen && <RegistrationWindow onSubmit={onSubmitRegistration} />}
+        <Header />
+        <Routes>
+          <Route path='/exercises' element={<ExercisesPage />} />
+          <Route path='/' element={<ProductsPage />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
